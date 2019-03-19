96bc211058efb4d5209a61dffdf260959720002c
Josep de Cid <josep.de.cid@gmail.com>
2019-03-19 15:07:27 +0300

    Remove scrapy and add windows paths

--- src/dataset/crawler.py
+++ src/dataset/crawler.py
@@ -1,35 +1,27 @@
+import logging
 import shutil
+from pathlib import Path
 from urllib import request
 
-import scrapy
-from scrapy import Selector
-from scrapy.crawler import CrawlerProcess
+from bs4 import BeautifulSoup
 
-from utils.constants import RAW_DATASET_PATH, SOURCE_MIDI_URLS, XPATH_URL_FILTER
+from utils.constants import RAW_DATASET_PATH, SOURCE_MIDI_URLS
 
+logging.getLogger().setLevel(logging.INFO)
 
-class MIDICrawler(scrapy.Spider):
-    name = 'MIDICrawler'
+for url in SOURCE_MIDI_URLS:
+    page = request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    download_link_tags = soup.find_all('a', attrs={'target': '_repro'}, href=True)
+    download_links = list(filter(lambda x: x[-4:] == '.mid', map(lambda x: x['href'], download_link_tags)))
 
-    def start_requests(self):
-        for url in SOURCE_MIDI_URLS:
-            yield scrapy.Request(url=url, callback=self.parse)
+    base_url = '/'.join(SOURCE_MIDI_URLS[0].split('/')[:-1])
+    for link in download_links:
+        file_name = link.split('/')[-1]
+        download_url = f'{base_url}/{link}'
 
-    def parse(self, response):
-        download_paths = Selector(response=response, type='html').xpath(XPATH_URL_FILTER).extract()
+        with request.urlopen(download_url) as response:
+            with open(Path(f'{RAW_DATASET_PATH}/{file_name}'), mode='wb') as f:
+                shutil.copyfileobj(response, f)
 
-        base_url = '/'.join(SOURCE_MIDI_URLS[0].split('/')[:-1])
-        for path in download_paths:
-            file_name = path.split('/')[-1]
-            download_url = f'{base_url}/{path}'
-
-            with request.urlopen(download_url) as response:
-                with open(f'{RAW_DATASET_PATH}/{file_name}', mode='wb') as f:
-                    shutil.copyfileobj(response, f)
-
-            self.logger.info(f'Downloaded and saved {file_name}')
-
-
-process = CrawlerProcess()
-process.crawl(MIDICrawler)
-process.start(stop_after_crawl=False)
+        logging.info(f'Downloaded and saved {file_name}')
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,8 +1,7 @@
 import os
 
 SOURCE_MIDI_URLS = ['http://www.jsbach.es/bbdd/index01_20.htm']
-XPATH_URL_FILTER = '//a[re:test(@target, "_repro") and re:test(@href, ".mid$")]//@href'
 
 PROJECT_PATH = os.getenv('BACHPROPAGATION_ROOT_PATH')
-DATASET_PATH = f'{PROJECT_PATH}/res/dataset/processed'
-RAW_DATASET_PATH = f'{PROJECT_PATH}/res/dataset/raw'
+DATASET_PATH = os.getenv('BACHPROPAGATION_ROOT_PATH') + '/res/dataset/processed'
+RAW_DATASET_PATH = os.getenv('BACHPROPAGATION_ROOT_PATH') + '/res/dataset/raw'
