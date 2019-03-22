6657ee75c73c0405bed763ec711c98b6aa9ca800
RufinaMay <rufa-gali@.mail.ru>
2019-03-23 01:04:23 +0300

    discriminator was added

--- src/model/GANDiscriminator.py
+++ src/model/GANDiscriminator.py
@@ -1,5 +1,5 @@
-from torch import nn
-
+from torch import nn#, log, mean
+from model.RNN import RNN
 
 class GANDiscriminator(nn.Module):
     def __init__(self, hid_dim: int):
@@ -11,5 +11,17 @@ class GANDiscriminator(nn.Module):
         """
         super(GANDiscriminator, self).__init__()
 
+        self.rnn = RNN(architecture='GRU', inp_dim=hid_dim, hid_dim=hid_dim, layers=2, bidirectional=True)
+        self.dense = nn.Linear(in_features=hid_dim, out_features=1)
+
     def forward(self, x):
-        pass
+        x = self.rnn(x)
+        x = self.dense(x)
+        return x
+
+    # def loss(self, fake, real):
+    #     fake_out = self.forward(fake)
+    #     real_out = self.forward(real)
+    #
+    #     loss = mean(-log(real_out) - log(1-fake_out))
+    #     return loss
