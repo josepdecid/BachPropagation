99ce6403fa93b3d2d67619607cfeb4ed8362c3c7
Josep de Cid <josep.de.cid@gmail.com>
2019-03-29 00:51:38 +0300

    Refactor structure

--- src/model/train.py
+++ src/model/train.py
@@ -6,40 +6,56 @@ from dataset.MusicDataset import MusicDataset
 from model.GANGenerator import GANGenerator
 from model.GANModel import GANModel
 from utils.constants import BATCH_SIZE, MAX_POLYPHONY
-from utils.typings import Optimizer, Criterion, NNet
 
 
-def train_generator(optimizer: Optimizer, criterion: Criterion, discriminator: NNet, fake_data):
+def train_generator(model: GANModel, data):
+    batch_size = data.size(0)
+    input_dim = data.size(1)
+
+    noise_data = GANGenerator.noise(batch_size, input_dim)
+    noise_data = noise_data.view(input_dim, batch_size, MAX_POLYPHONY)
+    fake_data = model.generator(noise_data)
+
     # Reset gradients
-    optimizer.zero_grad()
+    model.g_optimizer.zero_grad()
 
     # Forwards pass to get logits
-    prediction = discriminator(fake_data)
+    prediction = model.discriminator(fake_data)
 
     # Calculate gradients w.r.t parameters and backpropagate
-    loss = criterion(prediction, Variable(torch.ones(fake_data.size(0), 1)))
+    loss = model.g_criterion(prediction, Variable(torch.ones(fake_data.size(0), 1)))
     loss.backward()
 
     # Update parameters
-    optimizer.step()
+    model.g_optimizer.step()
     return loss
 
 
-def train_discriminator(optimizer: Optimizer, criterion: Criterion, discriminator: NNet, real_data, fake_data):
-    def train_data(data, fake: bool):
+def train_discriminator(model: GANModel, data):
+    def train_data(x, fake: bool):
         # Forwards pass to get logits
-        prediction = discriminator(data)
+        prediction = model.discriminator(x)
 
         # Calculate gradients w.r.t parameters and backpropagate
         n = real_data.size(0)
         target = Variable(torch.zeros(n)) if fake else Variable(torch.ones(n))
-        loss = criterion(prediction, target)
+        loss = model.d_criterion(prediction, target)
         loss.backward()
 
         return loss
 
+    batch_size = data.size(0)
+    input_dim = data.size(1)
+
+    real_data = Variable(data)
+    real_data = real_data.view(input_dim, batch_size, MAX_POLYPHONY)
+
+    noise_data = GANGenerator.noise(batch_size, input_dim)
+    noise_data = noise_data.view(input_dim, batch_size, MAX_POLYPHONY)
+    fake_data = model.generator(noise_data).detach()
+
     # Reset gradients
-    optimizer.zero_grad()
+    model.d_optimizer.zero_grad()
 
     # Train on real data
     loss_real = train_data(real_data, fake=False)
@@ -48,40 +64,15 @@ def train_discriminator(optimizer: Optimizer, criterion: Criterion, discriminato
     loss_fake = train_data(fake_data, fake=True)
 
     # Update parameters
-    optimizer.step()
+    model.d_optimizer.step()
     return loss_real + loss_fake
 
 
 def train_epoch(model: GANModel, loader: DataLoader) -> float:
     model.train_mode()
-    for batch in loader:
-        batch_size = batch.size(0)
-        input_dim = batch.size(1)
-
-        # Train Discriminator
-        real_data = Variable(batch)
-        real_data = real_data.view(input_dim, batch_size, MAX_POLYPHONY)
-
-        noise_data = GANGenerator.noise(batch_size, input_dim)
-        noise_data = noise_data.view(input_dim, batch_size, MAX_POLYPHONY)
-        fake_data = model.generator(noise_data).detach()
-
-        d_loss = train_discriminator(optimizer=model.d_optimizer,
-                                     criterion=model.d_criterion,
-                                     discriminator=model.discriminator,
-                                     real_data=real_data,
-                                     fake_data=fake_data)
-
-        # Train Generator
-        noise_data = GANGenerator.noise(batch_size, input_dim)
-        noise_data = noise_data.view(input_dim, batch_size, MAX_POLYPHONY)
-        fake_data = model.generator(noise_data)
-
-        g_loss = train_generator(optimizer=model.g_optimizer,
-                                 criterion=model.g_criterion,
-                                 discriminator=model.discriminator,
-                                 fake_data=fake_data)
-
+    for features in loader:
+        d_loss = train_discriminator(model=model, data=features)
+        g_loss = train_generator(model=model, data=features)
         return d_loss + g_loss
 
 
